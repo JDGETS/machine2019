1432f68afe857611807424ea8821acb872077e6c
becojo <benoit.cote.jodoin@gmail.com>
2018-12-15 19:25:09 -0500

    update tyro

--- tests/tyro_dyn/tyro_detendre.py
+++ tests/tyro_dyn/tyro_detendre.py
@@ -6,28 +6,32 @@ chain = DxlChain("/dev/ttyUSB0", rate=1000000)
 chain.open()
 
 
-chain.motors[250] = DxlMotor.instantiateMotor(250, 12)
+# chain.motors[250] = DxlMotor.instantiateMotor(250, 12)
 
-# chain.get_motor_list(broadcast=False)
+chain.get_motor_list()
 
-motor_id = 250
+
+motor_id = 1
+
+chain.set_reg(motor_id, 'cw_angle_limit', 0)
+chain.set_reg(motor_id, 'ccw_angle_limit', 0)
 
 chain.ping(motor_id)
 
-chain.set_reg(250, 'max_torque', 1023)
-chain.set_reg(250, 'torque_limit', 1023)
+chain.set_reg(motor_id, 'max_torque', 1023)
+chain.set_reg(motor_id, 'torque_limit', 1023)
 
 while True:
-    load = chain.get_reg(250, 'present_load')
+    load = chain.get_reg(motor_id, 'present_load')
     direction = load >> 10
     load = load % 1023
 
     print 'load = %d, dir = %d' % (load, direction)
 
     if load >= 9:
-        chain.set_reg(250, 'moving_speed', 1023 + 1024)
+        chain.set_reg(motor_id, 'moving_speed', 1023)
         time.sleep(1)
-        chain.set_reg(250, 'moving_speed', 0)
+        chain.set_reg(motor_id, 'moving_speed', 0)
         time.sleep(0.2)
 
     time.sleep(0.1)
