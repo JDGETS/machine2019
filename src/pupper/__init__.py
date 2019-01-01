e4b4e377bceff6bc6191086fe9552b4a14a09ec0
Marc Antoine Dumont <dumont.marcantoine13@gmail.com>
2019-01-01 17:10:13 -0500

    analyse image is in pupper main

--- src/pupper/__init__.py
+++ src/pupper/__init__.py
@@ -10,13 +10,4 @@ def main(instance):
     print 'waiting for connection to ' + instance + '...'
     wait_for_host(get_param('ip'))
 
-    stream_cmd = "mplayer -fps 200 -demuxer h264es ffmpeg://tcp://%s:9999 > /dev/null &"
-
-    if not os.getenv('NO_CAMERA'):
-        os.system(stream_cmd % get_param('ip'))
-
-        time.sleep(1)
-
-        os.system('''i3-msg '[class="MPlayer"] floating enable' ''')
-
     ui.main()
